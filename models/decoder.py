1eb977ca2510977cbf348a7f96887cc87e922f94
ArronHZG <hou.zg@foxmail.com>
2019-08-22 11:30:33 +0800

    Initial commit

---
+++ models/decoder.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+
+from models.utils import initialize_weights
+
+
+class Decoder(nn.Module):
+    def __init__(self, low_level_channels, num_classes):
+        super(Decoder, self).__init__()
+        self.conv1 = nn.Conv2d(low_level_channels, 48, 1, bias=False)
+        self.bn1 = nn.BatchNorm2d(48)
+        self.relu = nn.ReLU(inplace=True)
+
+        # Table 2, best performance with two 3x3 convs
+        self.output = nn.Sequential(
+            nn.Conv2d(48+256, 256, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.1),
+            nn.Conv2d(256, num_classes, 1, stride=1),
+        )
+        initialize_weights(self)
+
+    def forward(self, x, low_level_features):
+        low_level_features = self.conv1(low_level_features)
+        low_level_features = self.relu(self.bn1(low_level_features))
+        H, W = low_level_features.size(2), low_level_features.size(3)
+
+        x = F.interpolate(x, size=(H, W), mode='bilinear', align_corners=True)
+        x = self.output(torch.cat((low_level_features, x), dim=1))
+        return x
